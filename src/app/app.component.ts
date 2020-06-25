dd17b28eba33893793e76258f90cbc9156307b13
Anthony <anthonymarques@outlook.com>
2020-06-25 21:39:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './model/country.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flags';
+  // always use typeScript never any..or at least try to 
+  countries: Country[] = [
+    {
+      name: "South Africa",
+      code: "za",
+    },
+    {
+      name: "France",
+      code: "fr",
+    },
+    {
+      name: "Switzerland",
+      code: "ch",
+    },
+    {
+      name: "Greece",
+      code: "gr",
+    },
+    {
+      name: "Netherlands",
+      code: "nl",
+    },
+    {
+      name: "Germany",
+      code: "de",
+    },
+  ];
 }
